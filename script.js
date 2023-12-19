d20276fd2ea98952465bac9c05d1939fc4b4c9f0
jakelinecordova <jakeline.l.c.c@gmail.com>
2023-12-19 15:21:47 +0100

    subiendo ejercicios Numero 1

---
+++ script.js
@@ -0,0 +1,22 @@
+const sumarNumeros = (numero1, numero2) => numero1 + numero2;
+const resultado = sumarNumeros(5, 10); 
+console.log(resultado);
+
+const restarNumeros = (numero1, numero2) => numero1 - numero2;
+const resultado2 = restarNumeros(20, 10); 
+console.log(resultado2);
+
+const productoNumeros = (numero1, numero2) => numero1 * numero2;
+const resultado3 = productoNumeros(4, 5); 
+console.log(resultado3);
+
+const divisionNumeros = (numero1, numero2) => numero1 / numero2;
+const resultado4 = divisionNumeros(20, 5); 
+console.log(resultado4);
+
+const calcularPotencia = (base, exponente) => Math.pow(base, exponente);
+const base = 2;
+const exponente = 3;
+const resultado5 = calcularPotencia(base, exponente);
+console.log(resultado5);
+
